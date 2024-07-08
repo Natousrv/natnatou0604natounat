cc6336d33351df3f69667d14cbdd06c6e9552d18
Natou_nat <125877473+Natousrv@users.noreply.github.com>
2024-07-08 23:23:41 +0200

    Add files via upload

---
+++ install_cert.ps1
@@ -0,0 +1,12 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$CertFilePath
+)
+
+try {
+    Import-Certificate -FilePath $CertFilePath -CertStoreLocation Cert:\LocalMachine\Root -Confirm:$false -ErrorAction Stop
+    Write-Output "Certificat installé avec succès dans le magasin d'Autorités de certification racines."
+} catch {
+    Write-Error "Erreur lors de l'installation du certificat : $_"
+    exit 1
+}
