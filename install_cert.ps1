8088be9b8c05bd0d7a4fea09c86556224e9c9292
Natou_nat <125877473+Natousrv@users.noreply.github.com>
2024-07-09 00:55:03 +0200

    Update install_cert.ps1

--- install_cert.ps1
+++ install_cert.ps1
@@ -1,19 +1,19 @@
-param(
-    [Parameter(Mandatory=$true)]
-    [string]$CertFilePath
-)
+function Test-AdminRights {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())
+    return $currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
 
-try {
-    $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
-    $cert.Import($CertFilePath)
-
-    $store = New-Object System.Security.Cryptography.X509Certificates.X509Store("Root", "LocalMachine")
-    $store.Open("ReadWrite")
-    $store.Add($cert)
-    $store.Close()
-
-    Write-Output "Certificat installé avec succès dans le magasin d'Autorités de certification racines."
-} catch {
-    Write-Error "Erreur lors de l'installation du certificat : $_"
+# Vérifier si l'utilisateur a des droits administratifs
+if (Test-AdminRights) {
+    # Installer le certificat en tant qu'administrateur
+    try {
+        Import-Certificate -FilePath $CertFilePath -CertStoreLocation Cert:\LocalMachine\Root -Confirm:$false -ErrorAction Stop
+        Write-Output "Certificat installé avec succès dans le magasin d'Autorités de certification racines."
+    } catch {
+        Write-Error "Erreur lors de l'installation du certificat : $_"
+        exit 1
+    }
+} else {
+    Write-Error "Vous n'avez pas les droits administratifs nécessaires pour installer ce certificat."
     exit 1
 }
