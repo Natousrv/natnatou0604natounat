7fc0f5d659e73cf40ec2844a362b345313f24438
Natou_nat <125877473+Natousrv@users.noreply.github.com>
2024-07-09 00:32:37 +0200

    Update install_cert.ps1

--- install_cert.ps1
+++ install_cert.ps1
@@ -4,12 +4,7 @@ param(
 )
 
 try {
-    $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
-    $cert.Import($CertFilePath)
-    $store = New-Object System.Security.Cryptography.X509Certificates.X509Store("Root", "CurrentUser")
-    $store.Open("ReadWrite")
-    $store.Add($cert)
-    $store.Close()
+    certutil -addstore -user root $CertFilePath
     Write-Output "Certificat installé avec succès dans le magasin d'Autorités de certification racines de l'utilisateur actuel."
 } catch {
     Write-Error "Erreur lors de l'installation du certificat : $_"
